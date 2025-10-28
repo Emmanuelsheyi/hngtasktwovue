0d43ccdc1be894c3e045a34a9e5946b8008696d2
Emmanuelsheyi <esheyi8484@gmail.com>
2025-10-28 22:12:17 +0100

    added a new line of code

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   base: './',
+//   plugins: [vue()],
+// })
+
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [vue()],
+  base: './',  // 👈 this is key
 })
